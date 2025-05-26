1b428458edccb18485c53a76c589fb4944d6ac25
Zsolt Sándor <ezsolt@gmail.com>
2025-05-26 16:25:06 +0200

    misc

--- docs/app/main.js
+++ docs/app/main.js
@@ -1,11 +1,37 @@
 import DomReadyPromise from './resources/42/DomReadyPromise.js';
 import spx from './resources/spx/index.js'
+// import PartialLoader from './resources/42/PartialLoader.js';
+import { default as PartialLoader } from './resources/42/PartialLoader.js';
+
+import ScreensaverOverlay from './ui/screensaver/ScreensaverOverlay.js';
 
 (async () => {
     await DomReadyPromise.ready(); // Wait for DOMContentLoaded
     console.log('DOM is ready');
-    // do stuff
+      // Basic usage
+
+      // With custom configuration
+      const screensaver = new ScreensaverOverlay({
+        selector: '.screensaver',
+        defaultDelay: 3000, // 3 seconds between layout changes
+        defaultInactivityDelay: 60000, // 1 minute of inactivity
+        apiEndpoint: 'api/screensaver-layouts'
+      });
+
+      // Public methods
+      screensaver.forceShow(); // Show immediately
+      screensaver.forceHide(); // Hide and restart timer
+      screensaver.isScreensaverActive(); // Check if active
+      screensaver.destroy(); // Clean up when done
+      
 })();
+
+
+
+
+
+
+
 
 
 
@@ -24,6 +50,34 @@ const domReady = spx({
 // This is the main entry point for your app
 export default domReady(function(session) {
   // You initialize third party js in this callback
+
   console.log(session);
 
 })
+
+
+  // const { default: PartialLoader } = await import('./resources/42/PartialLoader.js');
+
+const loader = new PartialLoader();
+
+/*
+  const loader = new PartialLoader({
+    baseUrl: '/partials',
+    timeout: 5000,
+    cacheEnabled: true
+  });
+*/
+  // Load all partials in document
+  await loader.init();
+
+
+
+
+
+
+
+
+
+
+
+
