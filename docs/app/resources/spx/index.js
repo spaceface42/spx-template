2f74a51ac9db58921f918613be96c171acafa1a7
Zsolt Sándor <ezsolt@gmail.com>
2025-05-27 09:48:30 +0200

    typo fix - instead of .

--- docs/app/resources/spx/index.js
+++ docs/app/resources/spx/index.js
@@ -2,5 +2,5 @@
 // export * from "./0.0.5-rc.0/spx.mjs";
 // export { default } from "./0.0.5-rc.0/spx.mjs";
 
-export * from "./0.0.1.rc.1/index.js";
-export { default } from "./0.0.1.rc.1/index.js";
+export * from "./0.0.1-rc.1/index.js";
+export { default } from "./0.0.1-rc.1/index.js";
