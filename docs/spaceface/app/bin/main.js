9222facbe9ef7aa34cbc9eaffc1791367af9cc5f
Zsolt Sándor <ezsolt@gmail.com>
2025-06-07 21:19:55 +0200

    removing unused

--- docs/spaceface/app/bin/main.js
+++ docs/spaceface/app/bin/main.js
@@ -1,68 +1,26 @@
 import { eventBus } from '../../system/bin/EventBus.js';
 
+// const isDev = window.location.hostname === 'localhost' || window.location.hostname.includes('127.0.0.1');
 
-
-/*
-// v1
-const isDev = window.location.hostname === 'localhost' || window.location.hostname.includes('127.0.0.1');
-
-if (isDev) {
-  eventBus.on('log', ({ level = 'info', args = [] }) => {
-    switch (level) {
-      case 'error':
-        console.error('[app] ', ...args);
-        break;
-      case 'warn':
-        console.warn('[app] ', ...args);
-        break;
-      case 'info':
-      default:
-        console.log('[app] ', ...args);
-        break;
-    }
-  });
-}
-*/
-
-/*
-// v2
-// Set up event logging right away
 const eventsToLog = [
-  'screensaver:initialized',
-  'theme:randomLoaded',
-  'debug:enabled',
-  'serviceWorker:registered',
-  'partialLoader:initialized',
-  'page:homeInit',
-  'page:appInit',
-  'app:initComplete',
-  'app:initError',
-  'feature:initFailed',
+  '*',
 ];
 
-// Register listeners for these events to log them
-eventsToLog.forEach(eventName => {
-  eventBus.on(eventName, (...args) => {
-    logMessage('info', `[eventBus] Event: ${eventName}`, ...args);
+// if (isDev) {
+  eventsToLog.forEach(eventName => {
+    eventBus.on(eventName, (eventName, payload) => {
+      console.log(`████████ [ any ×͜× listener ] Event: ${eventName}`, payload ?? '(no payload)');
+    });
   });
-});
+// }
 
-// Optionally, listen to all events if your event bus supports `onAny`
-// eventBus.onAny((eventName, ...args) => {
-//   logMessage('info', `[eventBus] Event emitted: ${eventName}`, ...args);
-// });
-*/
-
-
-
-
-
-
+/*
+// v3
 // Listen to all events:
 eventBus.on('*', (eventName, payload) => {
   console.log(`████████ [ any ×͜× listener ] Event: ${eventName}`, payload ?? '(no payload)');
 });
-
+*/
 
 
 
