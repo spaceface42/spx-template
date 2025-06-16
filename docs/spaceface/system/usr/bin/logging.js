0a22a187b48dd5c116ad0f67e97d77bd63e1bb5c
Zsolt Sándor <ezsolt@gmail.com>
2025-06-16 16:19:51 +0200

    new logging function

--- docs/spaceface/system/usr/bin/logging.js
+++ docs/spaceface/system/usr/bin/logging.js
@@ -1,6 +1,6 @@
 import { eventBus } from '../../bin/EventBus.js';
 
 // for future event logs
-export function logMessage(level = 'info', ...args) {
-  eventBus.emit('log', { level, ...args });
+export function logMessage(level = 'info', message, details = {}) {
+  eventBus.emit('log', { level, message, details, timestamp: new Date().toISOString() });
 }
